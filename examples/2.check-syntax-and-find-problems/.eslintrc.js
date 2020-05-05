8429dff671fec88f5fbc5a2f8e467af91be2508a
GaryHjy <782316959@qq.com>
2020-05-05 14:30:23 +0800

    feat: init

---
+++ examples/2.check-syntax-and-find-problems/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars": "warn" // 可修改警告 0 = off, 1 = warn, 2 = error
+    }
+};
